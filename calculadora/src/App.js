508239d4d5e245db10c3b4762b1f3988bd71214f
leticialmbarros <armixletix@gmail.com>
2022-11-15 21:05:45 -0300

    projeto finalizado

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,50 +1,122 @@
-import Input from './components/Input';
-import Button from './components/Button';
+import Input from "./components/Input";
+import Button from "./components/Button";
 
+import { Container, Content, Row } from "./styles";
+import { useState } from "react";
 
+const App = () => {
+  const [currentNumber, setCurrentNumber] = useState("0");
 
+  const [firstNumber, setFirstNumber] = useState("0");
 
-import { Container, Content, Row, Column } from './styles';
+  const [operation, setOperation] = useState("");
 
+  const handleAddNumber = (num) => {
+    setCurrentNumber((prev) => `${prev === "0" ? "" : prev}${num}`);
+  };
 
+  const handleSumNumber = () => {
+    if (firstNumber === "0") {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber("0");
+      setOperation("+");
+    } else {
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(sum));
+      operation("");
+    }
+  };
+  const handleSubNumber = () => {
+    if (firstNumber === "0") {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber("0");
+      setOperation("-");
+    } else {
+      const sum = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(sum));
+      operation("");
+    }
+  };
+  const handleMultiNumber = () => {
+    if (firstNumber === "0") {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber("0");
+      setOperation("*");
+    } else {
+      const sum = Number(firstNumber) * Number(currentNumber);
+      setCurrentNumber(String(sum));
+      operation("");
+    }
+  };
+  const handleDivNumber = () => {
+    if (firstNumber === "0") {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber("0");
+      setOperation("/");
+    } else {
+      const sum = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(sum));
+      operation("");
+    }
+  };
 
-function App() {
+  const handleEquals = () => {
+    if (firstNumber !== "0" && operation !== "" && currentNumber !== "0") {
+      switch (operation) {
+        case "+":
+          handleSumNumber();
+          break;
+
+        case "-":
+          handleSubNumber();
+          break;
+        case "*":
+          handleMultiNumber();
+          break;
+        case "/":
+          handleDivNumber();
+          break;
+        default:
+          break;
+      }
+    }
+  };
+  const handleClear = () => {
+    setCurrentNumber("0");
+    setFirstNumber("0");
+    setOperation("");
+  };
   return (
     <Container>
       <Content>
-        <Input />
-        
+        <Input value={currentNumber} />
         <Row>
-        <Button label ="x" />
-        <Button label ="/" />
-        <Button label ="C" />
-        <Button label ="%" />
+          <Button label="/" onClick={handleDivNumber} />
+          <Button label="0" onClick={() => handleAddNumber("0")} />
+          <Button label="C" onClick={handleClear} />
+          <Button label="X" onClick={handleMultiNumber} />
         </Row>
-        
         <Row>
-        <Button label ="7" />
-        <Button label ="8" />
-        <Button label ="9" />
-        <Button label ="-" />
+          <Button label="7" onClick={() => handleAddNumber("7")} />
+          <Button label="8" onClick={() => handleAddNumber("8")} />
+          <Button label="9" onClick={() => handleAddNumber("9")} />
+          <Button label="-" onClick={handleSubNumber} />
         </Row>
-
         <Row>
-        <Button label ="4" />
-        <Button label ="5" />
-        <Button label ="6" />
-        <Button label ="+" />
+          <Button label="4" onClick={() => handleAddNumber("4")} />
+          <Button label="5" onClick={() => handleAddNumber("5")} />
+          <Button label="6" onClick={() => handleAddNumber("6")} />
+          <Button label="+" onClick={handleSumNumber} />
         </Row>
-
         <Row>
-        <Button label ="1" />
-        <Button label ="2" />
-        <Button label ="3" />
-        <Button label ="=" />
+          <Button label="1" onClick={() => handleAddNumber("1")} />
+          <Button label="2" onClick={() => handleAddNumber("2")} />
+          <Button label="3" onClick={() => handleAddNumber("3")} />
+          <Button label="=" onClick={handleEquals} />
         </Row>
-
       </Content>
     </Container>
   );
-}
+};
 
-export default App;
+export default App
